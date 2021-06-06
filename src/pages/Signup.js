8f10ec200ff335390ea33fc9550ae6e5ccd54d5e
vaibhavbuccha <jainvaibhav415@gmail.com>
2021-06-06 18:16:18 +0530

    'RegisterAPI'

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -6,22 +6,55 @@ import {
   InputAdornment,
   Button,
 } from "@material-ui/core";
-import React from "react";
+import React, { useRef, useState } from "react";
 import Layout from "./Layout";
 import style from "./Signup.module.css";
 import { AccountCircle } from "@material-ui/icons";
 import VpnKeyIcon from "@material-ui/icons/VpnKey";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function Signup() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+
+  const [validation, setValidation] = useState([]);
+
+  const submitHandler = () => {
+    var formErrors = [];
+    if (emailRef.current.value == "") {
+      alert("email");
+      formErrors.push("Email is required");
+    }
+    if (passwordRef.current.value == "") {
+      formErrors.push("Password is required");
+    }
+    if (firstNameRef.current.value == "") {
+      formErrors.push("firstName is required");
+    }
+    if (lastNameRef.current.value == "") {
+      formErrors.push("lastName is required");
+    }
+    console.log(formErrors);
+    setValidation(formErrors);
+  };
+
   return (
     <Layout>
+      <div>
+        {validation.map((error) => {
+          return <li>{error}</li>;
+        })}
+      </div>
       <div className={style.signupbox}>
         <Box m={2}>
           <h3 align="center">Register </h3>
           <FormControl fullWidth m={3}>
             <InputLabel>First name</InputLabel>
             <Input
+              ref={firstNameRef}
               id="input-with-icon-adornment"
               type="text"
               startAdornment={
@@ -36,6 +69,7 @@ export default function Signup() {
           <FormControl fullWidth m={3}>
             <InputLabel>Last name</InputLabel>
             <Input
+              ref={lastNameRef}
               id="input-with-icon-adornment"
               type="text"
               startAdornment={
@@ -50,8 +84,10 @@ export default function Signup() {
           <FormControl fullWidth m={3}>
             <InputLabel>Email</InputLabel>
             <Input
+              ref={emailRef}
               id="input-with-icon-adornment"
               type="email"
+              autocomplete="off"
               startAdornment={
                 <InputAdornment position="start">
                   <AccountCircle />
@@ -64,8 +100,10 @@ export default function Signup() {
           <FormControl fullWidth m={3}>
             <InputLabel>Password</InputLabel>
             <Input
+              ref={passwordRef}
               id="input-with-icon-adornment"
               type="password"
+              autocomplete="off"
               startAdornment={
                 <InputAdornment position="start">
                   <VpnKeyIcon />
@@ -76,7 +114,9 @@ export default function Signup() {
           <br />
           <br />
           <FormControl fullWidth>
-            <Button color="default">Sign Up</Button>
+            <Button color="default" onClick={submitHandler}>
+              Sign Up
+            </Button>
           </FormControl>
           <br />
           <p align="right">
