e1f071a172ba3278da5bc6b29e88e8040beed54b
vaibhavbuccha <jainvaibhav415@gmail.com>
2021-06-07 21:47:35 +0530

    'register'

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -15,114 +15,123 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default function Signup() {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const firstNameRef = useRef();
-  const lastNameRef = useRef();
-
-  const [validation, setValidation] = useState([]);
-
-  const submitHandler = () => {
-    var formErrors = [];
-    if (emailRef.current.value == "") {
-      alert("email");
-      formErrors.push("Email is required");
-    }
-    if (passwordRef.current.value == "") {
-      formErrors.push("Password is required");
-    }
-    if (firstNameRef.current.value == "") {
-      formErrors.push("firstName is required");
-    }
-    if (lastNameRef.current.value == "") {
-      formErrors.push("lastName is required");
-    }
-    console.log(formErrors);
-    setValidation(formErrors);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [response, setResponse] = useState("");
+  const [isError, setIsError] = useState(false);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/v1/admin/register", {
+        name: `${firstName} ${lastName}`,
+        email,
+        password,
+      })
+      .then((res) => {
+        setResponse(() => "Successfully Registered. ");
+        setIsError(false);
+      })
+      .catch((err) => {
+        setResponse(() => err.response.data.message);
+        setIsError(true);
+      });
   };
-
   return (
     <Layout>
-      <div>
-        {validation.map((error) => {
-          return <li>{error}</li>;
-        })}
-      </div>
       <div className={style.signupbox}>
-        <Box m={2}>
-          <h3 align="center">Register </h3>
-          <FormControl fullWidth m={3}>
-            <InputLabel>First name</InputLabel>
-            <Input
-              ref={firstNameRef}
-              id="input-with-icon-adornment"
-              type="text"
-              startAdornment={
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-          <br />
-          <br />
-          <FormControl fullWidth m={3}>
-            <InputLabel>Last name</InputLabel>
-            <Input
-              ref={lastNameRef}
-              id="input-with-icon-adornment"
-              type="text"
-              startAdornment={
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-          <br />
-          <br />
-          <FormControl fullWidth m={3}>
-            <InputLabel>Email</InputLabel>
-            <Input
-              ref={emailRef}
-              id="input-with-icon-adornment"
-              type="email"
-              autocomplete="off"
-              startAdornment={
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-          <br />
-          <br />
-          <FormControl fullWidth m={3}>
-            <InputLabel>Password</InputLabel>
-            <Input
-              ref={passwordRef}
-              id="input-with-icon-adornment"
-              type="password"
-              autocomplete="off"
-              startAdornment={
-                <InputAdornment position="start">
-                  <VpnKeyIcon />
-                </InputAdornment>
-              }
-            />
-          </FormControl>
-          <br />
-          <br />
-          <FormControl fullWidth>
-            <Button color="default" onClick={submitHandler}>
-              Sign Up
-            </Button>
-          </FormControl>
-          <br />
-          <p align="right">
-            <Link to="/">Allready have an account</Link>
-          </p>
-        </Box>
+        <div
+          align="center"
+          style={{
+            color: !isError ? "green" : "red",
+            border: !isError ? "1px solid green" : "1px solid red",
+            padding: "10px",
+            borderRadius: "10px",
+          }}
+        >
+          {response}
+        </div>
+        <form onSubmit={submitHandler}>
+          <Box m={2}>
+            <h3 align="center">Register </h3>
+            <FormControl fullWidth m={3}>
+              <InputLabel>First name</InputLabel>
+              <Input
+                onChange={(e) => setFirstName(e.target.value)}
+                id="input-with-icon-adornment"
+                type="text"
+                required
+                startAdornment={
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <br />
+            <br />
+            <FormControl fullWidth m={3}>
+              <InputLabel>Last name</InputLabel>
+              <Input
+                onChange={(e) => setLastName(e.target.value)}
+                id="input-with-icon-adornment"
+                type="text"
+                required
+                startAdornment={
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <br />
+            <br />
+            <FormControl fullWidth m={3}>
+              <InputLabel>Email</InputLabel>
+              <Input
+                onChange={(e) => setEmail(e.target.value)}
+                id="input-with-icon-adornment"
+                type="email"
+                autocomplete="off"
+                required
+                startAdornment={
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <br />
+            <br />
+            <FormControl fullWidth m={3}>
+              <InputLabel>Password</InputLabel>
+              <Input
+                onChange={(e) => setPassword(e.target.value)}
+                id="input-with-icon-adornment"
+                type="password"
+                autocomplete="off"
+                required
+                startAdornment={
+                  <InputAdornment position="start">
+                    <VpnKeyIcon />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <br />
+            <br />
+            <FormControl fullWidth>
+              <Button color="default" type="submit">
+                Sign Up
+              </Button>
+            </FormControl>
+            <br />
+            <p align="right">
+              <Link to="/">Allready have an account</Link>
+            </p>
+          </Box>
+        </form>
       </div>
     </Layout>
   );
